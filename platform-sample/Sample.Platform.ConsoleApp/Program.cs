21fde1bb74c0665418fb6960bcc433592ca1d832
Saeed Ganji <shahabganji@gmail.com>
2020-07-10 12:19:15 +0430

    add console app for platform sample

---
+++ platform-sample/Sample.Platform.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using static System.Console;
+
+namespace Sample.Platform
+{
+    public interface SampleCommand
+    {
+        Guid Id { get; set; }
+        string Command { get; set; }
+    }
+}
+
+
+namespace Sample.Platform.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq();
+
+            await bus.StartAsync();
+
+            while (true)
+            {
+                Write("Enter a command or leave it blank to quit: ");
+
+                var command = ReadLine();
+                
+                if( string.IsNullOrEmpty(command) ) break;
+
+                await bus.Publish<SampleCommand>(new { InVar.Id , Command = command });
+
+            }
+
+            await bus.StopAsync();
+        }
+    }
+}
